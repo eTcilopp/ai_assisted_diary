8ef361c29f636c1ce324ab704b4887d13f9afdad
eTcilopp <kirikeza@gmail.com>
2024-04-01 07:00:58 -0600

    minor changes

--- main.py
+++ main.py
@@ -588,7 +588,7 @@ def run():
 
 def main():
     scheduler = sched.scheduler(time.time, time.sleep)
-    interval = 60 * 60 * 4 # 6 hours in seconds
+    interval = (60 * 60 * 4)
 
     def periodic_task(sc):
         run()
